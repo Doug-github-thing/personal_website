d7843cf24086df7757b13551c884279f0b472f20
Doug-github-thing <KappaKeepo420!>
2024-01-17 20:47:09 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+A website to act as a personal project portfolio. 
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <h1>Doug</h1>
+            <p>Service Engineer II | Computer Engineering Graduate Student</p>
+        </header>
+        <div class="container">
+            <h2>About Me</h2>
+            <div class="bio">
+                <p>Hello! I am a Service Engineer at a pharmaceutical CDMO, and I am currently pursuing my a Masters in Computer Engineering. Now have a click on these buttons!</p>
+                <p>I love Carina all time.</p>
+            </div>
+            <div class="button-container">
+                <button onClick={() => { console.log("clicked!") }}>Click</button>
+                <a class="button" href="https://doug-github-thing.github.io/tictacslide/" target="iframe">TicTacSlide</a>
+                <a class="button" href="https://doug-github-thing.github.io/snake/" target="iframe">Snake</a>
+                <a class="button" href="https://github.com/Doug-github-thing/" target="_blank" rel="noopener noreferrer">My github</a>
+            </div>
+            <iframe name="iframe" frameborder="0" width="100%" height="400"></iframe>
+        </div>
     </div>
   );
 }
