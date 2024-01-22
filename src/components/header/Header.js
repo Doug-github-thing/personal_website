538f6be74d15b31b92a1e3aae69d44bfd6a5bc88
Doug-github-thing <KappaKeepo420!>
2024-01-22 15:13:16 -0500

    added link to About when clicking header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,15 @@
-const Header = () => {
+
+/**
+ * Renders the site header. If the user clicks the h1 title, it calls setSelected to
+ * set the state to the homepage (post #0).
+ * 
+ * @param {function} setSelected A callback function to set the currently selected post.  
+ * @returns 
+ */
+const Header = ({ setSelected }) => {
     return ( 
         <div>
-            <h1>Doug</h1>
+            <h1 onClick={() => setSelected("0")}>Doug</h1>
             <p>Service Engineer II | Computer Engineering Graduate Student</p>
         </div>
     );
