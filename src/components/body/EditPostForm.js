a029ff63dfa490d8b3ad0bd8ab51607ffa2c812d
Doug-github-thing <KappaKeepo420!>
2024-01-22 17:46:29 -0500

    edit post form actually sends data!

--- src/components/body/EditPostForm.js
+++ src/components/body/EditPostForm.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
 
+import { doc, setDoc, Timestamp } from "firebase/firestore";
+import db from "../MyFirestore";
+
 const EditPostForm = ({ post, cancelEditingStatus }) => {
 
     // Tracks input form status
     const [title, setTitle] = useState(post.title);
     const [content, setContent] = useState(post.content);
-    
 
-    const submit = () => {
-        console.log("Submit was clicked. Captured the following:");
-        console.log(`Title: ${title}`);
-        console.log(`Content: ${content}`);
+
+    // Called when the form submit is clicked
+    const submit = async () => {
+        // Make the change
+        let updatedPost = post;
+        updatedPost.title = title;
+        updatedPost.content = content;
+        updatedPost.timestamp = Timestamp.fromDate(new Date());
+
+        await setDoc(doc(db, "posts", `post${post.postId}`), updatedPost);
+
+        cancelEditingStatus();
     }
 
     const cancel = () => {
