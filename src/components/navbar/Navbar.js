2e3fbc1ca0746fe3e72453f8520eeecec7c426e4
Doug-github-thing <KappaKeepo420!>
2024-01-24 16:04:15 -0500

    supports new post, added more documentation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,17 +5,21 @@ import SiteLinkButtons from "./SiteLinkButtons";
  * A component that sits on the left side of the screen and contains links to "pages"
  * clickable by the user, which transfer state to the main body component to render
  * the appropriate page.
+ * 
+ * @param posts A representation of all post objects in the database.
+ * @param {function} setSelected A callback function to set the currently active post number
+ *                               in the app to the post that was clicked on.
  */
 const Navbar = ({ posts, setSelected }) => {
+
     return (
     <>
-
         <h1 className="posts-header">My Projects</h1>
 
         {/* List of post titles */}
         {
             posts?.map((this_post, index) => (
-                <div className="post-title" key={index} onClick={ () => setSelected(this_post.postId) }>
+                <div className="post-title" key={index} onClick={ () => setSelected(this_post.id) }>
                     {this_post.title}</div>
             ))
         }
@@ -25,7 +29,6 @@ const Navbar = ({ posts, setSelected }) => {
 
         <h1 className="links-header">Links</h1>
         <SiteLinkButtons />
-
     </>);
 }
 
